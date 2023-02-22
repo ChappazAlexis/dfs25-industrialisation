af19fd11fc0ee6c97839b36b1ef9b7093882884a
ChappazAlexis <83456592+ChappazAlexis@users.noreply.github.com>
2023-02-22 01:11:13 -0800

    commit

--- index.php
+++ index.php
@@ -1,6 +1,22 @@
-<?php 
-$x = 5;
-$y = 3;
-$somme = $x + $y;
-echo "$x + $y = $somme"; 
+<html>
+<body>
+<form action="" method="post">
+Valeur 1:
+<input type="text" name="no1"><br>
+Valeur 2:
+<input type="text" name="no2"><br>
+<input type="submit" name="submit" value="SUM "><br>
+</form>
+</body>
+</html>
+<?php
+$sum="";
+if(isset($_POST['submit']))
+{
+	$no1=$_POST['no1'];
+	$no2=$_POST['no2'];
+	$sum=$no1+$no2;
+	
+}
 ?>
+Resultat:<?php echo $sum; ?>
